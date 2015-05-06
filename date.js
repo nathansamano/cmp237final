fa19a8c2ab125dd181a24a3386fb4760d036855a
Nathan Samano <nsv9982@saintjoe.edu>
2015-05-06 13:26:57 -0400

    made changes for exam

--- date.js
+++ date.js
@@ -2,7 +2,7 @@ function findDate(string) {
   var dateTime = /(\d{1,2})-(\d{1,2})-(\d{4})/;
   var match = dateTime.exec(string);
   return new Date(Number(match[3]),
-                  Number(match[2]) - 1,
-                  Number(match[1]));
+                  Number(match[1]) - 1,
+                  Number(match[2]));
 }
-print(findDate("30-1-2003"));
+print(findDate("1-30-2003"));
